54f2803bfcefc70baad503a3dd4d1c83b25e462a
chasen-bettinger <chasen-bettinger@users.noreply.github.com>
2017-02-16 23:24:29 -0500

    Control React Component Updates When New Props Are Received

--- src/App.js
+++ src/App.js
@@ -2,56 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	
 	constructor() {
 		super();
-		this.state = {val: 0}
-		this.update = this.update.bind(this)
-	}	
+		this.state = {increasing: false}
+	}
 
 	update() {
-		this.setState( {
-			val: this.state.val + 1
-		})
+		ReactDOM.render(
+			<App val={this.props.val+1} />, 
+			document.getElementById('root'))
 	}
 
-	// Access to state and props
-	componentWillMount() {
-		console.log('componentWillMount');
-		this.setState({m: 2})
+	componentWillReceiveProps(nextProps) {
+		this.setState({increasing: nextProps.val > this.props.val})
 	}
-
-	componentDidMount() {
-		console.log('componentDidMount');
-		this.inc = setInterval(this.update,500);
+	shouldComponentUpdate(nextProps, nextState) {
+		return nextProps.val % 5 === 0;
 	}
 
 	render() {
-		console.log('render');
-		return <button onClick={this.update}>{this.state.val * this.state.m}</button>
-	}
-
-	componentWillUnmount() {
-		console.log('componentWillUnmount');
-		clearInterval(this.inc)
-	}
-}
-
-class Wrapper extends React.Component {
-	mount() {
-		ReactDOM.render(<App />, document.getElementById('a'))
-	}
-	unmount() {
-		ReactDOM.unmountComponentAtNode(document.getElementById('a'))
-	}
-	render() {
+		console.log(this.state.increasing)
 		return (
-			<div>
-			<button onClick={this.mount.bind(this)}>Mount</button>
-			<button onClick={this.unmount.bind(this)}>UnMount</button>
-			<div id="a"></div>
-			</div>
+			<button onClick={this.update.bind(this)}>
+			{this.props.val}
+			</button>
 		)
 	}
+
+	componentDidUpdate(prevProps, prevState) {
+		console.log('prevProps: ${prevProps.val}')
+	}
 }
 
-export default Wrapper 
+App.defaultProps = {val:0}
+export default App 
