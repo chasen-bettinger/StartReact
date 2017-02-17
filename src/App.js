d910ff3a9b64b5460745ea1949ac0eca9d5f4f5c
chasen-bettinger <chasen-bettinger@users.noreply.github.com>
2017-02-16 22:10:51 -0500

    ref to Get a Reference to Specific Components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 
 	constructor() {
 		super(); // going to give us the context of the parent
 		this.state = {
-			txt: 'this is the state txt'
+			a: ' '
 		}
 
 	}
 
 
-	update (e) { // custom method
-		this.setState({txt: e.target.value})
+	update () { // custom method
+		this.setState({
+			// gets the value for the specific node 'a'
+		a: this.a.refs.input.value,
+
+		// gets the value for the specific node 'b'
+		b: this.refs.b.value
+		})
+		
 	}
 
 
   render() {
     return (
     	<div>	
-    		<h1>{this.state.txt}</h1>
-    		<Widget update={this.update.bind(this)} />
+    		<Input 
+    		ref={component => this.a = component}
+    		update={this.update.bind(this)}
+    		/>
+    		{this.state.a}
+
+    		<hr />
+    		<input
+    		ref="b"
+    		type="text"
+    		onChange={this.update.bind(this)}
+    		/>
+    		{this.state.b}
     	</div>
     )
   }
 }
 
-// Stateless widget
-const Widget = (props) =>
-<input type="text" onChange={props.update}/>
-// Creating new keys
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
+
+class Input extends React.Component {
+	render() {
+		//Wrapping in a div now references the FindDOMNode call
+		return <div><input  ref="input" type="text" onChange={this.props.update}/></div>
+	}
 }
 
-// Creating default keys
-App.defaultProps = {
-	txt: "this is the default txt"
-}
 
-//const App = () => <h1>Hello stateless</h1>
 export default App 
