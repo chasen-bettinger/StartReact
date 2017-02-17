244045c84019123d9120af8430feafa419c07712
chasen-bettinger <chasen-bettinger@users.noreply.github.com>
2017-02-17 15:28:04 -0500

    Understand React.Children Utilities

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
 import React from 'react';
-import './App.css'
+
 
 class App extends React.Component {
-
-	constructor() {
-		super();
-		this.state = {
-			input: '/* add your jsx here */',
-			output: ' ',
-			err: ' '
-		}
-	}
-
-	update(e) {
-		let code = e.target.value;
-		try {
-			this.setState({
-				output: window.Babel
-				.transform(code, { presets: ['es2015', 'react']})
-				.code,
-				err: ''
-			})
-		}
-		catch(err) {
-			this.setState({err: err.message})
-		}
-	}
-
 	render() {
-		return(
-			<div>
-				<header>{this.state.err}</header>
-				<div className="container">
-					<textarea
-					onChange={this.update.bind(this)}
-					defaultValue={this.state.input}/>
-					<pre>
-						{this.state.output}
-					</pre>
-				</div>
-			</div>
+		return (
+			<Parent>
+				<div className="childA"></div>
+				<div className="childB"></div>
+			</Parent>
 		)
+	}
+}
+
+
+class Parent extends React.Component {
+	render() {
+		// console.log(this.props.children
+		//let items = React.Children
+		//.forEach(this.props.children,
+		//	child => console.log(child.props.className))
+
+		// will throw an error if there are two classes 
+		// as it stands, it will throw an error
+		let items = React.Children.only(this.props.children)
+
+		console.log(items)
+		return null
 	}
 }
 
